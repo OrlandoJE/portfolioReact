73cdd66f11a34e78bf412cec4f1c796d90d8ca4e
OrlandoJE <orlandoesparza6@gmail.com>
2024-06-27 16:33:32 -0700

    v1.4

--- src/components/Proyecto.tsx
+++ src/components/Proyecto.tsx
@@ -1,32 +1,29 @@
 import "./Proyecto.css";
 import { ButtonVisit } from "./ButtonVisit";
 
-import Logisnay from "../assets/Logisnay.png";
-
 interface ProyectoProps {
   id: number;
+  title: string;
+  subtitle: string;
+  description: string;
+  image: string;
+  imgTitle: string;
 }
 
 export const Proyecto = (props: ProyectoProps) => {
   return (
     <div className="Proyecto" id={props.id.toString()}>
-      {props.id % 2 === 0 ? (
+      {props.id % 2 === 1 ? (
         <>
           <img
-            src={Logisnay}
-            alt="Logisnay"
+            src={props.image}
+            alt={props.imgTitle}
             style={{ borderRadius: "20px 0px 0px 20px" }}
           />
           <div className="desc">
-            <h4>Página Web para Empresa de Logística</h4>
-            <h2>LogisNay</h2>
-            <p>
-              Desarrollo de un sitio web estático para la empresa de logística{" "}
-              <b>LogisNay</b>. La página proporciona información detallada sobre
-              los servicios de la empresa y permite a los usuarios enviar
-              correos electrónicos como método de contacto, utilizando PHP para
-              el envío de los mismos.
-            </p>
+            <h4>{props.subtitle}</h4>
+            <h2>{props.title}</h2>
+            <p>{props.description}</p>
             <div className="botones">
               <ButtonVisit class="primary" text="Visitar" />
               <ButtonVisit class="secundary" text="Conocer Más" />
@@ -36,23 +33,17 @@ export const Proyecto = (props: ProyectoProps) => {
       ) : (
         <>
           <div className="desc">
-            <h4>Página Web para Empresa de Logística</h4>
-            <h2>LogisNay</h2>
-            <p>
-              Desarrollo de un sitio web estático para la empresa de logística{" "}
-              <b>LogisNay</b>. La página proporciona información detallada sobre
-              los servicios de la empresa y permite a los usuarios enviar
-              correos electrónicos como método de contacto, utilizando PHP para
-              el envío de los mismos.
-            </p>
+            <h4>{props.subtitle}</h4>
+            <h2>{props.title}</h2>
+            <p>{props.description}</p>
             <div className="botones">
               <ButtonVisit class="primary" text="Visitar" />
               <ButtonVisit class="secundary" text="Conocer Más" />
             </div>
           </div>
           <img
-            src={Logisnay}
-            alt="Logisnay"
+            src={props.image}
+            alt={props.imgTitle}
             style={{ borderRadius: "0px 20px 20px 0px" }}
           />
         </>
--- src/components/Proyectos.tsx
+++ src/components/Proyectos.tsx
@@ -1,14 +1,24 @@
 import "./Proyectos.css";
 
 import { Proyecto } from "./Proyecto";
+import proyectosData from '../proyectos.json'
+
 
 export const Proyectos = () => {
   return (
     <div className="Proyectos" id="projects">
       <h2>Proyectos</h2>
-      <Proyecto id={0} />
-      <Proyecto id={1} />
-
+      {proyectosData.map((proyecto) => (
+        <Proyecto
+          key={proyecto.id || 'Error'}
+          id={proyecto.id || 1}
+          title={proyecto.title || 'Error'}
+          subtitle={proyecto.subtitle || 'Error'}
+          description={proyecto.description || 'Error'}
+          image={proyecto.image || 'Error'}
+          imgTitle={proyecto.imgTitle || 'Error'}
+        />
+      ))}
     </div>
   );
 };
